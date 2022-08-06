faf4d2e5ae4a6f1d89521b73e1a907777328c3ee
TJ Phan <t.jamesphan@gmail.com>
2022-08-05 17:53:08 -0700

    Update all files due to corruption preventing nest element generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 import { CustomersModule } from './customers/customers.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CustomersModule],
-  // providers: [CustomersService],
+  imports: [CustomersModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
